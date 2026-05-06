c4dae30d9dfbf0b15a34f18cf35002fdf8042b31
M7MDB <m7mdbox1@gmail.com>
2026-05-06 18:18:49 +0300

    add overall progress card and update dashboard styles and functionality

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,43 @@
-const filterCards = document.querySelectorAll('.filter_card');
+filterButton();
+summary_values()
+progressBar()
 
-filterCards.forEach(card => {
-    const buttons = card.querySelectorAll('.filter_btn');
+function progressBar() {
+    const progressBars = document.querySelectorAll(".overall_progress");
 
-    buttons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            e.preventDefault();
+    progressBars.forEach(progress => {
+        const value = 66; // This value should be dynamically calculated based on the tasks data
+        progress.dataset.progress = value;
+        progress.style.setProperty("--progress", `${value * 3.6}deg`);
+    });
+}
 
-            buttons.forEach(btn => btn.classList.remove('active'));
+function summary_values() {
+    const total_tasks_value = document.querySelector('.total_tasks_value');
+    const completed_tasks_value = document.querySelector('.completed_tasks_value');
+    const pending_tasks_value = document.querySelector('.pending_tasks_value');
+    const late_tasks_value = document.querySelector('.late_tasks_value');
 
-            button.classList.add('active');
+    total_tasks_value.textContent = 0;
+    completed_tasks_value.textContent = 0;
+    pending_tasks_value.textContent = 0;
+    late_tasks_value.textContent = 0;
+}
+
+function filterButton() {
+    const filterCards = document.querySelectorAll('.filter_card');
+
+    filterCards.forEach(card => {
+        const buttons = card.querySelectorAll('.filter_btn');
+
+        buttons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                buttons.forEach(btn => btn.classList.remove('active'));
+
+                button.classList.add('active');
+            });
         });
     });
-});
+}
