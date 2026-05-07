a59e93f52f9fcb780eb8fc3b518c6bcd55201ce2
M7MDB <m7mdbox1@gmail.com>
2026-05-07 08:04:45 +0300

    refactor: enhance dashboard styles and add individual tasks section with dynamic data

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,50 @@
+const percentageValue = 55;
+const tasksInformations = 5;
+
+
+const taskData = {
+    courseName: "Computer Networks",
+    taskStatus: "Completed",
+    taskTitle: "TCP/IP Protocol",
+    progressPercentage: 55,
+    taskDate: "2024-06-30",
+    daysLeft: 5
+};
+
 filterButton();
 summary_values()
 progressBar()
+individualCardInformation()
+
+function individualCardInformation() {
+
+    const courseName = document.querySelector('.course_name_individual_task_card');
+    const taskStatus = document.querySelector('.status_task_individual_task_card');
+    const taskTitle = document.querySelector('.task_title_individual');
+    const progressPercentage = document.querySelector('.progress_percentage');
+    const taskDate = document.querySelector('.date_task_individual');
+    const daysLeft = document.querySelector('.days_left_individual');
+
+
+    courseName.textContent = taskData.courseName;
+
+    taskStatus.textContent = taskData.taskStatus;
+
+    taskTitle.textContent = taskData.taskTitle;
+
+    progressPercentage.textContent = `${taskData.progressPercentage}%`;
+
+    taskDate.textContent = taskData.taskDate;
+
+    daysLeft.textContent = `${taskData.daysLeft} days left`;
+
+}
 
 function progressBar() {
     const progressBars = document.querySelectorAll(".overall_progress");
 
     progressBars.forEach(progress => {
-        const value = 66; // This value should be dynamically calculated based on the tasks data
+        const value = percentageValue; // This value should be dynamically calculated based on the tasks data
         progress.dataset.progress = value;
         progress.style.setProperty("--progress", `${value * 3.6}deg`);
     });
@@ -18,10 +56,10 @@ function summary_values() {
     const pending_tasks_value = document.querySelector('.pending_tasks_value');
     const late_tasks_value = document.querySelector('.late_tasks_value');
 
-    total_tasks_value.textContent = 0;
-    completed_tasks_value.textContent = 0;
-    pending_tasks_value.textContent = 0;
-    late_tasks_value.textContent = 0;
+    total_tasks_value.textContent = tasksInformations;
+    completed_tasks_value.textContent = tasksInformations;
+    pending_tasks_value.textContent = tasksInformations;
+    late_tasks_value.textContent = tasksInformations;
 }
 
 function filterButton() {
